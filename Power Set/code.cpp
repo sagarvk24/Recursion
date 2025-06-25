9c0209ad666ac646a8feda5de325022e2943485e
sagarvk24 <sagarsaan21@gmail.com>
2025-06-25 07:57:33 +0530

    code added for power set

--- Power Set/code.cpp
+++ Power Set/code.cpp
@@ -2,7 +2,45 @@
 #include <vector>
 using namespace std;
 
+void helperFunction(int index, vector<int> &nums, vector<int> &ds, vector<vector<int>> &list)
+{
+    if (index == nums.size())
+    {
+        list.push_back(ds);
+        return;
+    }
+
+    // including the current element
+    ds.push_back(nums[index]);
+    helperFunction(index + 1, nums, ds, list);
+    ds.pop_back();
+    // Not including
+    helperFunction(index + 1, nums, ds, list);
+}
+
+vector<vector<int>> powerSet(vector<int> &nums)
+{
+    vector<vector<int>> answer;
+    vector<int> ds;
+
+    helperFunction(0, nums, ds, answer);
+
+    return answer;
+}
 int main()
 {
+    vector<int> numArray = {1,2,3};
+
+    vector<vector<int>> result = powerSet(numArray);
+
+    for(const auto &subset: result)
+    {
+        cout<<"[";
+        for(int num: subset)
+        {
+            cout<<num<<" ";
+        }
+        cout<<"]"<<endl;
+    }
     return 0;
 }
